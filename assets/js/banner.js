0f6068f7e6764064733bfc58dcf23cf6b03f2447
MathisLanceRichardot <153006795+MathisLanceRichardot@users.noreply.github.com>
2025-04-15 19:02:01 +0200

    Delete assets directory

--- assets/js/banner.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let menuButton = document.querySelector(".menu-button");
-    let banner = document.querySelector(".banner");
-
-    menuButton.addEventListener("click", function () {
-        banner.classList.toggle("show"); // Affiche ou cache la bannière
-    });
-});
